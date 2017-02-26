c7ea733cafd1f7e0fd6ddc5419c7a6cc61378327
Nat <nat@cmmc.io>
2017-02-26 12:31:59 +0700

    export MicroGear

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let AUTH = () => { }
 import {Util} from './src/Util'
 import {NetpieAuth} from './src/NetpieAuth'
+import {MicroGear} from './src/MicroGear'
 
-export {Util, NetpieAuth};
+export {Util, NetpieAuth, MicroGear};
