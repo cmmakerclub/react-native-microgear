ab56eaa9c86fb445ad4a446a6ab6469d35ba26c8
Nat <nat@cmmc.io>
2017-02-27 13:35:23 +0700

    Fix for android.

--- examples/netpie_demo/index.android.js
+++ examples/netpie_demo/index.android.js
@@ -4,50 +4,162 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
 
+
+import {MicroGear} from 'react-native-microgear'
+const appid = "Goose";
+
+let thermo = require("./thermometer.png");
+
 export default class netpie_demo extends Component {
-  render() {
+  constructor () {
+    super();
+
+    this.microgear = MicroGear.create({
+      key: 'rO8JeDs3SGALNQI',
+      secret: 'X78Z18tX09vDI44fErSlKqmix',
+      alias: "alias"
+    });
+
+    this.state = {
+      temp: 0,
+      height: 405,
+      marginTop: 0,
+      color: 'cornflowerblue'
+    }
+
+  }
+
+  setTemp (c: number) {
+    let resultHeightPx = (405 * c) / 100;
+    let resultMarginTop = 405 - resultHeightPx + 30;
+    this.setState({height: resultHeightPx, marginTop: resultMarginTop});
+    this.setState({temp: c});
+
+  }
+
+  componentWillMount () {
+    this.microgear.on("connected", (...args) => {
+      console.log("netpie connected");
+      let counter = 0
+      this.microgear.subscribe("/gearname/#").then(() => {
+        console.log('subscribe..')
+      });
+
+      let interval = setInterval(() => {
+        let text = `${++counter} ok: ${new Date().getTime()}`
+        console.log("publishing..", text);
+        if (this.microgear.isConnected()) {
+          let random = (Math.round(Math.random() * 100) % 100)
+          // microgear.publish("/gearname/hello", text)
+          this.microgear.chat("temp", `${random}`);
+        }
+        else {
+          clearInterval(interval)
+        }
+      }, 1500)
+    });
+
+    this.microgear.on("message", (message) => {
+      console.log(`topic: ${message.destinationName}, payload: ${message.payloadString}`);
+      this.setTemp(parseFloat(message.payloadString))
+    });
+
+    this.microgear.on("disconnected", () => {
+      console.log("Disconnected...")
+      setTimeout(() => {
+        this.microgear.connect(appid)
+      }, 2000)
+    })
+  }
+
+  componentDidMount () {
+    this.setTemp(0)
+    setTimeout(() => {
+      this.setTemp(30)
+    }, 500)
+    this.microgear.connect(appid)
+  }
+
+
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <View style={styles.contentLeft}>
+          <Image source={ thermo } style={styles.thermo}>
+            <View style={[styles.heightTemp, {marginTop: this.state.marginTop}, {height: this.state.height}]}></View>
+            <View style={styles.defaultTemp}></View>
+          </Image>
+        </View>
+        <View style={styles.contentRight}>
+          <Text style={styles.dataTemp}>{this.state.temp} &#176;C</Text>
+        </View>
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    // justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'mistyrose',
+    // borderColor: 'red',
+    // borderWidth: 4
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  contentLeft: {
+    // flex: 1,
+    // borderColor: 'blue',
+    // borderWidth: 4
+    marginLeft: 30
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  contentRight: {
+    flex: 1,
+    alignItems: 'center',
+    // borderColor: 'green',
+    // borderWidth: 4
   },
+  thermo: {},
+  dataTemp: {
+    fontSize: 80,
+    fontWeight: '100'
+    // borderColor: 'green',
+    // borderWidth: 4
+  },
+  heightTemp: {
+    // marginTop: 30,
+    marginLeft: 45,
+    // borderColor: 'yellow',
+    // borderWidth: 3,
+    // height: 375,
+    width: 20,
+    backgroundColor: 'cornflowerblue',
+    position: 'absolute',
+    borderRadius: 10
+  },
+  defaultTemp: {
+    // flex: 1,
+    borderColor: 'cornflowerblue',
+    borderWidth: 4,
+    marginTop: 424,
+    marginLeft: 33,
+    width: 44,
+    height: 44,
+    borderRadius: 50,
+    backgroundColor: 'cornflowerblue'
+  }
 });
+
 
 AppRegistry.registerComponent('netpie_demo', () => netpie_demo);
